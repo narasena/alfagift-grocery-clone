ee2172f2a390f862e19a9140afcfac9738ff0e74
cursemaker <cursedsena@gmail.com>
2025-05-22 20:39:07 +0700

    feat(product-category): create data for seed

---
+++ finpro-api/src/data/products/productCategory.ts
@@ -0,0 +1,12 @@
+const productCategories = {
+  'Kebutuhan Dapur': [],
+  'Kebutuhan Ibu & Anak': [],
+  'Kebutuhan Rumah': [],
+  Makanan: [],
+  Minuman: [],
+  'Produk Segar & Beku': [],
+  'Personal Care': [],
+  'Kebutuhan Kesehatan': [],
+  Lifestyle: [],
+  'Pet Foods': [],
+};
