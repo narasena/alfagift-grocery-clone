680ce918f603b973541df313cc06edc90e363d6a
cursemaker <cursedsena@gmail.com>
2025-05-22 16:28:32 +0700

    feat(prisma): create data for seed

---
+++ finpro-api/prisma/data/products/productCategory.ts
@@ -0,0 +1,16 @@
+const productCategories = [
+    'Kebutuhan Dapur',
+    'Kebutuhan Ibu & Anak',
+    'Kebutuhan Rumah',
+    'Makanan',
+    'Minuman',
+    'Produk Segar & Beku',
+    'Personal Care',
+    'Kebutuhan Kesehatan',
+    'Lifestyle',
+    'Pet Foods'
+]
+
+const productSubCategories = {
+    K
+}
