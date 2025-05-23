44854343ba4f2f15e358f68a5b9e0ef1dd408a61
cursemaker <cursedsena@gmail.com>
2025-05-23 16:09:47 +0700

    feat(product): create product data

---
+++ finpro-api/src/data/products/products.ts
@@ -0,0 +1,18 @@
+export const products = [
+    {
+        name: 'Hi-Cook Tabung Gas Mini Portabel 230 g',
+        price: 28200,
+        productSubCategoryId:1
+    },
+    {
+        name: 'Piring Plastik 20 pcs',
+        price: 19100,
+        productSubCategoryId:1
+    },
+    {
+        name: 'Bright Gas Elpiji (Refill) 5.5 kg',
+        price: 98000,
+        productSubCategoryId:1
+    }
+    
+]
