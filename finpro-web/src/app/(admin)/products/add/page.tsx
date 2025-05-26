5ccafa762ad0f31018d5e03c8d964f59ce1b5c8f
cursemaker <cursedsena@gmail.com>
2025-05-26 16:03:29 +0700

    feat(add product): create form to add new product

--- finpro-web/src/app/(admin)/products/add/page.tsx
+++ finpro-web/src/app/(admin)/products/add/page.tsx
@@ -1,25 +1,51 @@
-'use client'
+'use client';
 import * as React from 'react';
-import { Field, Form, Formik } from 'formik';
+import { ErrorMessage, Field, Form, Formik } from 'formik';
 import apiInstance from '@/services/apiInstance';
+import { IProductCategory, IProductSubCategory } from '@/types/product.category/product.subcategory.type';
 
+type TAddProductField = {
+  name: string;
+  title: string;
+  type: string;
+  options?: IProductSubCategory[];
+};
+
+type TProductFormValues = {
+  name: string
+  price: number;
+  productCategorySubId: number;
+  brandId?: number;
+  description?: string;
+  sku?: string;
+  barcode?: string;
+  weight?: number;
+  dimensions?: string; 
+}
 export default function AddProductPage() {
-  const [productCategories, setProductCategories] = React.useState([]);
+  const [productCategories, setProductCategories] = React.useState<IProductCategory[]>([]);
+  const [productSubCategories, setProductSubCategories] = React.useState<IProductSubCategory[]>([]);
   const handleGetProductCategories = async () => {
     try {
-      const response = await apiInstance.get('/product-category/subcategories')
-      console.log(response.data)
-      setProductCategories(response.data)
+      const categoryResponse = await apiInstance.get('/product-category');
+      const subCategoryResponse = await apiInstance.get('/product-category/subcategories');
+      setProductCategories(categoryResponse.data.productCategories);
+      setProductSubCategories(subCategoryResponse.data.productSubCategories);
     } catch (error) {
       console.error('Error fetching product categories:', error);
-      
     }
-  }
-  const addProductFields = [
-    { name: 'name', type: 'text' },
-    { name: 'price', type: 'number' },
-    { name: 'productCategorySubId', type:'select', options: productCategories}, // This should be populated with categories
-  ]
+  };
+  const addProductFields: TAddProductField[] = [
+    { name: 'name', title: 'Product Name', type: 'text' },
+    { name: 'price', title: 'Product Price', type: 'number' },
+    { name: 'productCategorySubId', title: 'Product Sub Category', type: 'select', options: productSubCategories },
+    { name: 'brandId', title: 'Brand', type: 'select', options: [] },
+    { name: 'description', title: 'Product Description', type: 'text' },
+    { name: 'sku', title: 'SKU', type: 'text' },
+    { name: 'barcode', title: 'Barcode', type: 'text' },
+    { name: 'weight', title: 'Weight', type: 'number' },
+    { name: 'dimensions', title: 'Dimensions', type: 'text' }, // This should be populated with categories
+  ];
   React.useEffect(() => {
     handleGetProductCategories();
   }, []);
@@ -29,29 +55,53 @@ export default function AddProductPage() {
         initialValues={{
           name: '',
           price: 0,
-          productCategorySubId: 0
-        }}
-        onSubmit={()=> console.log('Form submitted')}
-      >
+          productCategorySubId: 0,
+          brandId: undefined,
+          description: '',
+          sku: '',
+          barcode: '',
+          weight: undefined,
+          dimensions: '',
+        } as TProductFormValues}
+        onSubmit={() => console.log('Form submitted')}>
         <Form>
           <div>
             {addProductFields.map((field, index) => (
-              <div key={index}>
-                <label htmlFor={field.name}>{field.name}</label>
-                {field.type === 'select' ? (
-                  <Field name={field.name} id={field.name} as='select'>
-                    {field.options!.map((option, optionIndex) => (
-                      <option key={optionIndex} value={option.}>{option.label}</option>
+              <div key={index} className='text-gray-700 px-2 w-full'>
+                <label htmlFor={field.name} className='block mb-2 text-2xl font-medium'>
+                  {field.title}
+                </label>
+                {field.type === 'select' && field.options ? (
+                  <Field
+                    name={field.name}
+                    id={field.name}
+                    as='select'
+                    className='w-full bg-white border border-gray-300 rounded-md p-2'>
+                    {productCategories.map((category) => (
+                      <optgroup key={category.id} label={category.name}>
+                        {productSubCategories
+                          .filter((subCategory) => subCategory.productCategoryId === category.id)
+                          .map((subCategory) => (
+                            <option key={subCategory.id} value={subCategory.id}>
+                              {subCategory.name}
+                            </option>
+                          ))}
+                      </optgroup>
                     ))}
                   </Field>
                 ) : (
-                  <Field type={field.type} name={field.name} id={field.name} />
+                  <Field
+                    type={field.type}
+                    name={field.name}
+                    id={field.name}
+                    className='w-full bg-white border border-gray-300 rounded-md p-2'
+                  />
                 )}
+                <ErrorMessage name={field.name} component='div' className='text-sm text-red-600' />
               </div>
             ))}
           </div>
         </Form>
-
       </Formik>
     </div>
   );
--- finpro-web/src/types/product.category/product.subcategory.type.ts
+++ finpro-web/src/types/product.category/product.subcategory.type.ts
@@ -1,7 +1,10 @@
-export interface IProductSubCategory {
+export interface IProductCategory {
     id: number
     name: string
     slug: string
     description: string
+}
+
+export interface IProductSubCategory extends IProductCategory{
     productCategoryId: number
 }
