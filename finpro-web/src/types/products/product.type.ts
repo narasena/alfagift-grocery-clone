f5d8a6d8a3d5595b7ea0bec873f25aa5628e3884
cursemaker <cursedsena@gmail.com>
2025-05-29 15:00:05 +0700

    feat(type): add product details type for web

--- finpro-web/src/types/products/product.type.ts
+++ finpro-web/src/types/products/product.type.ts
@@ -1,11 +1,13 @@
-import { IProductSubCategory } from "./product.subcategory.type";
+import { IProductBrand } from './product.brand.type';
+import { IProductSubCategory } from './product.category.type';
+import { IProductImage } from './product.image.type';
 
 export interface IAddProductField {
   name: string;
   title: string;
   type: string;
   options?: IProductSubCategory[];
-};
+}
 
 export interface IProduct {
   id: number;
@@ -24,10 +26,16 @@ export interface IProduct {
 }
 export interface IProductFormValues extends IProduct {
   images: ICloudinaryResult[];
-};
+}
 
 export interface ICloudinaryResult {
   public_id: string;
   secure_url: string;
   isMainImage: boolean;
-};
+}
+
+export interface IProductDetails extends IProduct {
+  productImage: IProductImage[]
+  productSubCategory: IProductSubCategory & { productCategory: IProductSubCategory }
+  productBrand: IProductBrand
+}
