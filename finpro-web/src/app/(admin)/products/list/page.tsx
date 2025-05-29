479817a218c288c9b2e183a88cb8393f732e3322
cursemaker <cursedsena@gmail.com>
2025-05-29 09:30:22 +0700

    feat(product list): create view page for product list

---
+++ finpro-web/src/app/(admin)/products/list/page.tsx
@@ -0,0 +1,20 @@
+import * as React from 'react';
+import { toast } from 'react-toastify';
+
+export default function AdminProductListViewPage() {
+    const [products, setProducts] = React.useState([]);
+
+    const handleGetProducts = async () => {
+        try {
+            
+        } catch (error) {
+            console.error('Error fetching products:', error);
+            toast.error('Failed to fetch products. Please try again later.');
+        }
+    }
+  return (
+    <div>
+      
+    </div>
+  );
+}
