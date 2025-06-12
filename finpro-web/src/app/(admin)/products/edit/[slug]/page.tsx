c6b16a7a682c1d2b34db1fb4fb6f162204e3a660
cursemaker <cursedsena@gmail.com>
2025-06-12 16:44:37 +0700

    fix: edit product page for changing product name-slug

--- finpro-web/src/app/(admin)/products/edit/[slug]/page.tsx
+++ finpro-web/src/app/(admin)/products/edit/[slug]/page.tsx
@@ -2,9 +2,9 @@
 import AdminPageTitle from "@/features/admin/components/AdminPageTitle";
 import ProductImageUploadWidget from "@/features/admin/products/components/ProductImageUploadWidget";
 import ProductInputFields from "@/features/admin/products/components/ProductInputFields";
-import { useEditProductImage } from "@/features/admin/products/edit/components/useEditProductImage";
+import { useEditProductImage } from "@/features/admin/products/edit/hooks/useEditProductImage";
 import { useEditProduct } from "@/features/admin/products/edit/hooks/useEditProduct";
-import { addProductSchemas } from "@/features/schemas/addProductSchemas";
+import { formProductSchema } from "@/features/admin/products/schemas/formProductSchema";
 import { IProductDetails } from "@/types/products/product.type";
 import { Form, Formik } from "formik";
 import { CldImage } from "next-cloudinary";
@@ -20,186 +20,183 @@ export default function EditProductPage(props: IAppProps) {
   const {
     product,
     allImagesList,
-    uploadedImages,
     handleImageClick,
     handleSwapImage,
     handleDeleteImage,
     handleImageUploadSuccess,
     imageShowing,
     handleSetAsMainImage,
-    isThumbnailSameWithImageShowing
+    isThumbnailSameWithImageShowing,
   } = useEditProductImage();
-  const { handleSaveChanges } = useEditProduct();
-  console.log(allImagesList);
+  const { handleSaveChanges } = useEditProduct(allImagesList);
 
   return (
-    <div className="bg-red-400">
+    <div className="">
       <div className="lg:bg-white lg:max-w-[1200px] lg:mx-auto lg:px-10 lg:py-4 px-4">
         <AdminPageTitle title="Edit Product" subTitle="Edit the form below to edit a product" />
-        <div className="lg: grid lg:grid-cols-[60%_1fr] w-full gap-6">
-          <div className="lg:order-2 lg:!px-0 max-w-full flex flex-col max-lg:justify-center items-center c-border">
-            <div className="page-title border-b border-gray-300 pb-4 mb-4">
-              <span className="px-6">Product Images</span>
-            </div>
-            {imageShowing && (
-              <div className="size-72 border border-gray-400 rounded-md my-3 overflow-hidden">
-                <CldImage
-                  width={288}
-                  height={288}
-                  src={"secure_url" in imageShowing ? imageShowing.secure_url : imageShowing.imageUrl}
-                  alt="Selected Product Image"
-                  className="w-full h-full object-cover"
-                />
-              </div>
-            )}
-            <div className="flex justify-center">
-              {allImagesList.length > 0 && imageShowing && imageShowing.isMainImage === false && (
-                <button
-                  className="my-4 px-4 py-2 bg-red-800 text-white font-semibold text-xl rounded-md"
-                  onClick={() => {
-                    handleSetAsMainImage();
-                    console.log("clicked");
-                  }}
-                >
-                  Set As Main Image
-                </button>
-              )}
-              {allImagesList.length > 0 && imageShowing && imageShowing.isMainImage === true && (
-                <button className="my-4 px-4 py-2 bg-gray-700 text-white font-semibold text-xl rounded-md">
-                  Main Image
-                </button>
-              )}
-            </div>
-            <div className="px-4 flex justify-center gap-2">
-              <div className="max-w-full flex justify-normal flex-wrap gap-4">
-                {allImagesList.map((image, index) => (
-                  <div key={index} className={``}>
-                    <div
-                      className={`max-lg:size-18 lg:size-28 border border-gray-400 rounded-md relative group !overflow-hidden ${
-                        isThumbnailSameWithImageShowing(image, imageShowing!) ? "ring-2 ring-red-600" : ""
-                      }`}
-                      onClick={() => handleImageClick(image.data)}
-                    >
-                      <CldImage
-                        fill
-                        sizes="(max-width: 768px) 72px, 112px"
-                        src={"secure_url" in image.data ? image.data.secure_url : image.data.imageUrl}
-                        alt={`Uploaded Image ${index + 1}`}
-                        className="object-cover z-0"
-                      />
-                      {index === 0 && (
-                        <div className="absolute top-0 left-0 bg-red-600 text-white text-xs px-1">Main</div>
-                      )}
-                      <div className="absolute top-0 right-0 min-w-full min-h-full bg-gray-800/20 opacity-0 group-hover:opacity-100 group-hover:z-[20]"></div>
-                      <div className="absolute size-max top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 group-hover:z-30">
-                        <button
-                          className="py-0.5 px-2 text-xs bg-red-700 text-white rounded-xs font-medium centered cursor-pointer"
-                          type="button"
-                          onClick={(e) => {
-                            e.stopPropagation();
-                            handleDeleteImage(
-                              "public_id" in image.data ? image.data.public_id : image.data.cldPublicId || ""
-                            );
-                          }}
-                        >
-                          <span>Delete </span>
-                          <MdDeleteForever />
-                        </button>
-                      </div>
-                    </div>
-                    <div className="grid grid-cols-2 gap-1 mt-1">
-                      <div className="col-start-1 flex-1">
-                        {index > 0 && (
-                          <button
-                            type="button"
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              handleSwapImage(index, index - 1);
-                            }}
-                            className="bg-gray-700 text-gray-200 w-full flex items-center justify-center text-2xl rounded-sm"
-                          >
-                            <TbArrowBigLeftLinesFilled />
-                          </button>
-                        )}
-                      </div>
-                      <div className="col-start-2 flex-1">
-                        {index < allImagesList.length - 1 && (
-                          <button
-                            type="button"
-                            onClick={(e) => {
-                              e.stopPropagation();
-                              handleSwapImage(index, index + 1);
-                            }}
-                            className="bg-gray-200 text-gray-700 w-full flex items-center justify-center text-2xl rounded-sm"
-                          >
-                            <TbArrowBigRightLinesFilled />
-                          </button>
-                        )}
-                      </div>
-                    </div>
-                  </div>
-                ))}
-                {
-                  <ProductImageUploadWidget
-                    uploadePreset="products-image"
-                    onSuccess={handleImageUploadSuccess}
-                    maxFiles={5}
-                    buttonText="Upload Images"
-                    type="thumbnails"
-                    uploadedImagesCount={allImagesList.length}
-                  />
-                }
-              </div>
-            </div>
-          </div>
-          <div className="lg:order-1">
-            <Formik
-              enableReinitialize={true}
-              initialValues={
-                {
-                  name: product?.name || "",
-                  price: product?.price || 0,
-                  productSubCategoryId: product?.productSubCategoryId || 0,
-                  brandId: product?.brandId || "",
-                  description: product?.description || "",
-                  sku: product?.sku || "",
-                  barcode: product?.barcode || "",
-                  weight: product?.weight || 0,
-                  dimensions: product?.dimensions || "",
-                  images: product?.productImage || [],
-                } as Partial<IProductDetails>
+        <Formik
+          enableReinitialize={true}
+          initialValues={
+            {
+              name: product?.name || "",
+              price: product?.price || 0,
+              productSubCategoryId: product?.productSubCategoryId || 0,
+              brandId: product?.brandId || "",
+              description: product?.description || "",
+              sku: product?.sku || "",
+              barcode: product?.barcode || "",
+              weight: product?.weight || 0,
+              dimensions: product?.dimensions || "",
+              images: product?.productImage || [],
+            } as Partial<IProductDetails>
+          }
+          validationSchema={formProductSchema}
+          onSubmit={(values, { setSubmitting, resetForm }) => {
+            console.log("Form Values:", values);
+            console.log("Is Valid:", formProductSchema.isValidSync(values));
+            try {
+              formProductSchema.validateSync(values, { abortEarly: false });
+              console.log("Validation passed!");
+              handleSaveChanges(values, resetForm);
+              setSubmitting(false);
+            } catch (err) {
+              if (err instanceof Yup.ValidationError) {
+                console.log("Validation Errors:", err.errors);
               }
-              validationSchema={addProductSchemas}
-              onSubmit={(values, { setSubmitting, resetForm }) => {
-                console.log("Form Values:", values);
-                console.log("Is Valid:", addProductSchemas.isValidSync(values));
-                try {
-                  addProductSchemas.validateSync(values, { abortEarly: false });
-                  console.log("Validation passed!");
-                  handleSaveChanges(values, resetForm);
-                  setSubmitting(false);
-                } catch (err) {
-                  if (err instanceof Yup.ValidationError) {
-                    console.log("Validation Errors:", err.errors);
-                  }
-                  setSubmitting(false);
-                }
-              }}
-            >
-              <Form>
-                <ProductInputFields />
-                <div className="px-3 w-full">
-                  <button
-                    type="submit"
-                    className="w-full mt-4 px-4 py-2 bg-red-800 text-white font-semibold text-2xl rounded-md "
-                  >
-                    Update Product
-                  </button>
+              setSubmitting(false);
+            }
+          }}
+        >
+          <Form className="lg: grid lg:grid-cols-[60%_1fr] w-full gap-6">
+            <div className="lg:order-2 max-w-full">
+              <div className="c-border lg:!px-0 flex flex-col max-lg:justify-center items-center">
+                <div className="page-title border-b border-gray-300 pb-4 mb-4">
+                  <span className="px-6">Product Images</span>
                 </div>
-              </Form>
-            </Formik>
-          </div>
-        </div>
+                {imageShowing && (
+                  <div className="size-72 border border-gray-400 rounded-md my-3 overflow-hidden">
+                    <CldImage
+                      width={288}
+                      height={288}
+                      src={"secure_url" in imageShowing ? imageShowing.secure_url : imageShowing.imageUrl}
+                      alt="Selected Product Image"
+                      className="w-full h-full object-cover"
+                    />
+                  </div>
+                )}
+                <div className="flex justify-center">
+                  {allImagesList.length > 0 && imageShowing && imageShowing.isMainImage === false && (
+                    <button
+                      className="my-4 px-4 py-2 bg-red-800 text-white font-semibold text-xl rounded-md"
+                      onClick={() => {
+                        handleSetAsMainImage();
+                        console.log("clicked");
+                      }}
+                    >
+                      Set As Main Image
+                    </button>
+                  )}
+                  {allImagesList.length > 0 && imageShowing && imageShowing.isMainImage === true && (
+                    <button className="my-4 px-4 py-2 bg-gray-700 text-white font-semibold text-xl rounded-md">
+                      Main Image
+                    </button>
+                  )}
+                </div>
+                <div className="px-4 flex justify-center gap-2">
+                  <div className="max-w-full flex justify-normal flex-wrap gap-4">
+                    {allImagesList.map((image, index) => (
+                      <div key={index} className={``}>
+                        <div
+                          className={`max-lg:size-18 lg:size-28 border border-gray-400 rounded-md relative group !overflow-hidden ${
+                            isThumbnailSameWithImageShowing(image, imageShowing!) ? "ring-2 ring-red-600" : ""
+                          }`}
+                          onClick={() => handleImageClick(image.data)}
+                        >
+                          <CldImage
+                            fill
+                            sizes="(max-width: 768px) 72px, 112px"
+                            src={"secure_url" in image.data ? image.data.secure_url : image.data.imageUrl}
+                            alt={`Uploaded Image ${index + 1}`}
+                            className="object-cover z-0"
+                          />
+                          {index === 0 && (
+                            <div className="absolute top-0 left-0 bg-red-600 text-white text-xs px-1">Main</div>
+                          )}
+                          <div className="absolute top-0 right-0 min-w-full min-h-full bg-gray-800/20 opacity-0 group-hover:opacity-100 group-hover:z-[20]"></div>
+                          <div className="absolute size-max top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2 opacity-0 group-hover:opacity-100 group-hover:z-30">
+                            <button
+                              className="py-0.5 px-2 text-xs bg-red-700 text-white rounded-xs font-medium centered cursor-pointer"
+                              type="button"
+                              onClick={(e) => {
+                                e.stopPropagation();
+                                handleDeleteImage(
+                                  "public_id" in image.data ? image.data.public_id : image.data.cldPublicId || ""
+                                );
+                              }}
+                            >
+                              <span>Delete </span>
+                              <MdDeleteForever />
+                            </button>
+                          </div>
+                        </div>
+                        <div className="grid grid-cols-2 gap-1 mt-1">
+                          <div className="col-start-1 flex-1">
+                            {index > 0 && (
+                              <button
+                                type="button"
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  handleSwapImage(index, index - 1);
+                                }}
+                                className="bg-gray-700 text-gray-200 w-full flex items-center justify-center text-2xl rounded-sm"
+                              >
+                                <TbArrowBigLeftLinesFilled />
+                              </button>
+                            )}
+                          </div>
+                          <div className="col-start-2 flex-1">
+                            {index < allImagesList.length - 1 && (
+                              <button
+                                type="button"
+                                onClick={(e) => {
+                                  e.stopPropagation();
+                                  handleSwapImage(index, index + 1);
+                                }}
+                                className="bg-gray-200 text-gray-700 w-full flex items-center justify-center text-2xl rounded-sm"
+                              >
+                                <TbArrowBigRightLinesFilled />
+                              </button>
+                            )}
+                          </div>
+                        </div>
+                      </div>
+                    ))}
+                    {
+                      <ProductImageUploadWidget
+                        onSuccess={handleImageUploadSuccess}
+                        maxFiles={5}
+                        buttonText="Upload Images"
+                        type="thumbnails"
+                        uploadedImagesCount={allImagesList.length}
+                      />
+                    }
+                  </div>
+                </div>
+              </div>
+              <div className="px-3 w-full">
+                <button
+                  type="submit"
+                  className="w-full mt-4 px-4 py-2 bg-red-800 text-white font-semibold text-2xl rounded-md "
+                >
+                  Update Product
+                </button>
+              </div>
+            </div>
+            <div className="lg:order-1">
+              <ProductInputFields />
+            </div>
+          </Form>
+        </Formik>
       </div>
     </div>
   );
--- finpro-web/src/features/admin/products/edit/hooks/useEditProduct.ts
+++ finpro-web/src/features/admin/products/edit/hooks/useEditProduct.ts
@@ -1,40 +1,49 @@
 import { IProductFormValues } from "@/types/products/product.type";
-import { useEditProductImage } from "../components/useEditProductImage";
+import { TImageItem, useEditProductImage } from "./useEditProductImage";
 import { ICloudinaryResult, IProductImage } from "@/types/products/product.image.type";
 import apiInstance from "@/utils/api/apiInstance";
 import { useProductDetails } from "@/hooks/products/useProductDetails";
 import { toast } from "react-toastify";
+import { useRouter } from "next/navigation";
 
-export const useEditProduct = () => {
-    const {product, refreshProductDetails} = useProductDetails()
-    const {allImagesList} = useEditProductImage();
-    const handleSaveChanges = async (values: Partial<IProductFormValues>, resetForm: () => void) => {
-      const existingImages = allImagesList
-        .filter((img) => img.type === "existing")
-        .map((img) => img.data as IProductImage);
+export const useEditProduct = (allImagesList: TImageItem[]) => {
+  const router = useRouter();
+  const { product, refreshProductDetails } = useProductDetails();
+  const handleSaveChanges = async (values: Partial<IProductFormValues>, resetForm: () => void) => {
+    console.log("All Images List at upload:", allImagesList);
+    const editedExistingImages = allImagesList
+      .filter((img) => img.type === "existing")
+      .map((img) => img.data as IProductImage);
 
-      const newUploads = allImagesList.filter((img) => img.type === "new").map((img) => img.data as ICloudinaryResult);
+    const newUploads = allImagesList.filter((img) => img.type === "new").map((img) => img.data as ICloudinaryResult);
 
-      try {
-        const editProductResponse = await apiInstance.put(`/product/edit/${product?.slug}`, {
-          ...values,
-          productSubCategoryId: Number(values.productSubCategoryId),
-          brandId: values.brandId || null,
-          description: values.description || null,
-          sku: values.sku || null,
-          barcode: values.barcode || null,
-          weight: Number(values.weight) || null,
-          dimensions: values.dimensions || null,
-          existingImages,
-          newUploads,
-        });
-        console.log("Edit Product Response:", editProductResponse.data);
-        toast.success("Product edited successfully!");
-        resetForm();
+    try {
+      const newSlug = values.name ? values.name.toLowerCase().replace(/\s+/g, "-") : product?.slug;
+      const editProductResponse = await apiInstance.put(`/product/edit/${product?.slug}`, {
+        ...values,
+        newSlug,
+        productSubCategoryId: Number(values.productSubCategoryId),
+        brandId: values.brandId || null,
+        description: values.description || null,
+        sku: values.sku || null,
+        barcode: values.barcode || null,
+        weight: Number(values.weight) || null,
+        dimensions: values.dimensions || null,
+        editedExistingImages,
+        newUploads,
+      });
+      console.log("Edit Product Response:", editProductResponse.data);
+      toast.success("Product edited successfully!");
+      resetForm();
+      if (newSlug !== product?.slug) {
+        router.push(`/products/edit/${editProductResponse.data.resultProduct.slug}`);
+      } else {
         refreshProductDetails();
-      } catch (error) {
-        console.error("Error editing product:", error);
       }
+    } catch (error) {
+      console.error("Error editing product:", error);
+      toast.error("Failed to edit product. Please try again.");
+    }
   };
-  return {handleSaveChanges}
-}
+  return { handleSaveChanges };
+};
