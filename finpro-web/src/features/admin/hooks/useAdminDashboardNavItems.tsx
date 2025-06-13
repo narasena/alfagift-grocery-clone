aae042d3cd1555eed6b52166484b7ba461faf400
cursemaker <cursedsena@gmail.com>
2025-06-13 10:31:29 +0700

    feat: add admin nav bars

---
+++ finpro-web/src/features/admin/hooks/useAdminDashboardNavItems.tsx
@@ -0,0 +1,26 @@
+import { HiHome } from "react-icons/hi";
+import { BiSolidReport } from "react-icons/bi";
+import { AiFillProduct } from "react-icons/ai";
+import { GrUserAdmin } from "react-icons/gr";
+import { RiDiscountPercentFill } from "react-icons/ri";
+import { usePathname } from "next/navigation";
+import { FaUsersCog } from "react-icons/fa";
+import { IoSettings } from "react-icons/io5";
+
+export const useAdminDashboardNavItems = () => {
+  const bottomNav = [
+    { name: "Home", href: "/dashboard", icon: <HiHome /> },
+    { name: "Reports", href: "/reports", icon: <BiSolidReport /> },
+    { name: "Products", href: "/products", icon: <AiFillProduct /> },
+    { name: "Promo", href: "/promos", icon: <RiDiscountPercentFill /> },
+    { name: "Admin", href: "/admin", icon: <GrUserAdmin /> },
+  ];
+  const sideNav = [
+    ...bottomNav,
+    { name: "Users", href: "/users", icon: <FaUsersCog /> },
+    { name: "Settings", href: "/settings", icon: <IoSettings /> },
+  ];
+    const pathName = usePathname();
+    
+    return { bottomNav, sideNav, pathName };
+};
