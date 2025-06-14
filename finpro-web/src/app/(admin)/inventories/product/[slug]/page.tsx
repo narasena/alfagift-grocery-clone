23a60b8a15dc621ab42f5105af4661b2ca74c012
cursemaker <cursedsena@gmail.com>
2025-06-14 21:58:37 +0700

    feat: create table list view for product stocks per product

--- finpro-web/src/app/(admin)/inventories/product/[slug]/page.tsx
+++ finpro-web/src/app/(admin)/inventories/product/[slug]/page.tsx
@@ -1,10 +1,26 @@
-import * as React from 'react';
+"use client";
+import { useAdminProductStocksPerProduct } from "@/features/admin/inventories/product/hooks/useAdminProductStocksPerProduct";
+import AdminListTable from "@/features/admin/products/list/components/AdminListTable";
+import { IProductStockTable } from "@/types/inventories/product.stock.type";
+import * as React from "react";
 
-
-export default function AdminInventoryByProductPage () {
+export default function AdminInventoryByProductPage() {
+  const { productStocks, productStocksListColumnTitles, getProductStocksCellValue } = useAdminProductStocksPerProduct();
+  console.log(productStocks);
   return (
     <div>
-      
+      {productStocks.length > 0 && (
+        <AdminListTable
+          title={`Product Stocks of: ${productStocks[0]?.product.name || ""}`}
+          tableDescription={`View product page `}
+          linkHref={`/p/${productStocks[0]?.product.slug}` || "#"}
+          linkLabel={`here.`}
+          columns={productStocksListColumnTitles}
+          data={productStocks as IProductStockTable[]}
+          withCheckbox={true}
+          renderCell={getProductStocksCellValue}
+        />
+      )}
     </div>
   );
 }
