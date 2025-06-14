74de4e32a48fe6d6441eb0f398a483d764d2924a
cursemaker <cursedsena@gmail.com>
2025-06-14 12:40:07 +0700

    feat: create product stock type

---
+++ finpro-web/src/types/inventories/product.stock.type.ts
@@ -0,0 +1,13 @@
+import { IProductDetails } from "../products/product.type"
+import { IStore } from "../stores/store.type"
+
+export interface IProductStock {
+    productId: string
+    product: IProductDetails
+    storeId: string
+    store: IStore
+    stock: number
+    createdAt: Date
+    updatedAt: Date
+    deletedAt?: Date
+}
