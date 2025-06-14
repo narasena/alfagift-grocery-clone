1432e5606401a00960837941ca9c9e33a5c128e6
cursemaker <cursedsena@gmail.com>
2025-06-14 12:39:14 +0700

    fix: get all stock controller to include product and store data

--- finpro-api/src/controllers/inventory.controller/inventory.controller.ts
+++ finpro-api/src/controllers/inventory.controller/inventory.controller.ts
@@ -3,7 +3,13 @@ import { NextFunction, Request, Response } from "express";
 
 export const getAllStocks = async (req: Request, res: Response, next: NextFunction) => {
     try {
-        const stocks = await prisma.productStock.findMany({where: {deletedAt: null}});
+        const stocks = await prisma.productStock.findMany({
+            where: { deletedAt: null },
+            include: {
+                product: true,
+                store: true
+            }
+        });
         res.status(200).json({
             success: true,
             message: "Stocks fetched successfully",
