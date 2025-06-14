2cc1518087e74179481be8a85281b48a8fd67185
cursemaker <cursedsena@gmail.com>
2025-06-14 22:06:53 +0700

    feat: create store stocks hook

---
+++ finpro-web/src/features/admin/inventories/store/hooks/useGetProductStocksPerStore.ts
@@ -0,0 +1,24 @@
+import { IProductStock } from "@/types/inventories/product.stock.type";
+import apiInstance from "@/utils/api/apiInstance";
+import { useParams } from "next/navigation";
+import * as React from "react";
+import { toast } from "react-toastify";
+
+export const useGetProductStocksPerStore = () => {
+  const { storeId } = useParams();
+  const [storeStocks, setStoreStocks] = React.useState<IProductStock[]>([]);
+  const handleGetProductStocksPerStore = async () => {
+    try {
+      const storeStocksResponse = await apiInstance.get("inventories/store/" + storeId);
+      setStoreStocks(storeStocksResponse.data.storeStocks);
+      toast.success(storeStocksResponse.data.message);
+    } catch (error) {
+      console.error("Error fetching product stocks per store:", error);
+      toast.error("Error fetching product stocks per store");
+    }
+  };
+  React.useEffect(() => {
+    handleGetProductStocksPerStore();
+  }, []);
+  return { storeStocks };
+};
