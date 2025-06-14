86111aaee3e2d2f8bcf42765debbd1e549e51ef8
cursemaker <cursedsena@gmail.com>
2025-06-14 18:25:00 +0700

    fix: return stocks response to include deletedAt null

--- finpro-api/src/controllers/inventory.controller/inventory.controller.ts
+++ finpro-api/src/controllers/inventory.controller/inventory.controller.ts
@@ -1,22 +1,50 @@
+import { Prisma, PrismaClientExtends } from "@prisma/client";
 import { prisma } from "../../prisma";
 import { NextFunction, Request, Response } from "express";
 
 export const getAllStocks = async (req: Request, res: Response, next: NextFunction) => {
-    try {
-        const stocks = await prisma.productStock.findMany({
-            where: { deletedAt: null },
-            include: {
-                product: true,
-                store: true
-            }
-        });
-        res.status(200).json({
-            success: true,
-            message: "Stocks fetched successfully",
-            stocks
-        });
-    } catch (error) {
-        next(error);
-        
-    }
-}
+  try {
+    const stocks = await prisma.productStock.findMany({
+      where: {
+        deletedAt: null,
+        product: {
+          deletedAt: null,
+          productSubCategory: {
+            deletedAt: null,
+            productCategory: {
+              deletedAt: null,
+            },
+          },
+          productBrand: {
+            deletedAt: null,
+          },
+        },
+        store: { deletedAt: null },
+      },
+      include: {
+        product: {
+          include: {
+            productImage: {
+              where: { deletedAt: null },
+              orderBy: [{ isMainImage: "desc" }, { updatedAt: "desc" }],
+            },
+            productSubCategory: {
+              include: {
+                productCategory: true
+              }
+            },
+            productBrand: true
+          },
+        },
+        store: true,
+      },
+    });
+  res.status(200).json({
+    success: true,
+    message: "Stocks fetched successfully",
+    stocks,
+  })
+  } catch (error) {
+    next(error);
+  }
+};
