22d00e2aaa937ac8744049076e6c240669d9724b
cursemaker <cursedsena@gmail.com>
2025-06-16 19:51:31 +0700

    feat: create single product store stock hook

--- finpro-web/src/features/admin/components/AdminTable.tsx
+++ finpro-web/src/features/admin/components/AdminTable.tsx
@@ -1,12 +1,13 @@
 import Link from "next/link";
 import * as React from "react";
+import AdminTableHeader from "./AdminTableHeader";
 
 export interface ITableColumn {
   key: string;
   label: string;
 }
 
-interface IAdminListTableProps<T> {
+export interface IAdminTableProps<T> {
   title?: string;
   tableDescription?: string;
   linkHref?: string;
@@ -14,7 +15,7 @@ interface IAdminListTableProps<T> {
   columns: ITableColumn[];
   data: T[];
   withCheckbox?: boolean;
-  renderCell?: (row: T, key: string) => React.ReactNode;
+  renderCell?: (row: T, key: string) => React.ReactNode | undefined;
 }
 
 export default function AdminTable<T extends Record<string, unknown>>({
@@ -26,21 +27,11 @@ export default function AdminTable<T extends Record<string, unknown>>({
   data,
   withCheckbox,
   renderCell,
-}: IAdminListTableProps<T>) {
+}: IAdminTableProps<T>) {
   return (
     <div className="relative !overflow-auto shadow-lg sm:rounded-lg">
       <table className="w-full text-sm text-left text-gray-500">
-        <caption className="p-5 text-lg font-semibold text-left text-gray-900 bg-white">
-          {title}
-          <p className="mt-1 text-sm font-normal text-gray-500">
-            {tableDescription}
-            {linkHref && (
-              <Link href={linkHref} className="font-medium text-blue-600 hover:underline">
-                {linkLabel}
-              </Link>
-            )}
-          </p>
-        </caption>
+        <AdminTableHeader title={title} tableDescription={tableDescription} linkHref={linkHref} linkLabel={linkLabel} />
         <thead className="text-xs text-gray-700 uppercase bg-gray-200">
           <tr>
             {withCheckbox && (
@@ -67,18 +58,20 @@ export default function AdminTable<T extends Record<string, unknown>>({
         <tbody>
           {data.map((row, rowIndex) => (
             <tr key={rowIndex} className="bg-white border-b border-gray-200 hover:bg-gray-50">
-              <td className="w-4 p-4">
-                <div className="flex items-center">
-                  <input
-                    id={`checkbox-table-search-${rowIndex}`}
-                    type="checkbox"
-                    className="size-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2"
-                  />
-                  <label htmlFor={`checkbox-table-search-${rowIndex}`} className="sr-only">
-                    checkbox
-                  </label>
-                </div>
-              </td>
+              {withCheckbox && (
+                <td className="w-4 p-4">
+                  <div className="flex items-center">
+                    <input
+                      id={`checkbox-table-search-${rowIndex}`}
+                      type="checkbox"
+                      className="size-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2"
+                    />
+                    <label htmlFor={`checkbox-table-search-${rowIndex}`} className="sr-only">
+                      checkbox
+                    </label>
+                  </div>
+                </td>
+              )}
               {columns.map((col, colIndex) => (
                 <td key={colIndex} className={col.key === "image" ? "p-1.5" : "px-6 py-4"}>
                   {renderCell ? renderCell(row, col.key) : String(row[col.key] ?? "—")}
