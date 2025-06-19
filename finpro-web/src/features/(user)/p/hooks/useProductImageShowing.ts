3975a66510eb97845203046116c4d3e4c6d83e25
al-ethea <alethea.azka3@gmail.com>
2025-06-19 12:36:57 +0700

    fix: image showing type to include icloudinary result

--- finpro-web/src/features/(user)/p/hooks/useProductImageShowing.ts
+++ finpro-web/src/features/(user)/p/hooks/useProductImageShowing.ts
@@ -1,13 +1,12 @@
-import { IProductImage } from "@/types/products/product.image.type";
+import { ICloudinaryResult, IProductImage } from "@/types/products/product.image.type";
 import * as React from "react";
 
 export const useProductImageShowing = () => {
-    const [imageShowing, setImageShowing] = React.useState<IProductImage | null>(null);
-    const handleImageClick = (image: IProductImage) => setImageShowing(image);
-    return {
-        imageShowing,
-        setImageShowing,
-        handleImageClick
-    }
-    
-}
+  const [imageShowing, setImageShowing] = React.useState<IProductImage | ICloudinaryResult | null>(null);
+  const handleImageClick = (image: IProductImage) => setImageShowing(image);
+  return {
+    imageShowing,
+    setImageShowing,
+    handleImageClick,
+  };
+};
