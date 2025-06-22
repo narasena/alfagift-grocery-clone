6896b22da1e0c3edcf8a666b99bd6b8092f149d3
cursemaker <cursedsena@gmail.com>
2025-06-22 17:01:16 +0700

    feat: create basic form for add discount

---
+++ finpro-web/src/app/(admin)/discounts/add/page.tsx
@@ -0,0 +1,101 @@
+import { EDiscountType } from "@/types/discounts/discount.type";
+import * as React from "react";
+
+export default function AdminAddDiscountPage() {
+  const [discountType, setDiscountType] = React.useState<string>("")
+  const discountTypes = Object.keys(EDiscountType)
+
+  return (
+    <section className="bg-white dark:bg-gray-900">
+      <div className="py-8 px-4 mx-auto max-w-2xl lg:py-16">
+        <h2 className="mb-4 text-xl font-bold text-gray-900 dark:text-white">Add a new Discount Promo</h2>
+        <form action="#">
+          <div className="grid gap-4 sm:grid-cols-2 sm:gap-6">
+            <div className="sm:col-span-2">
+              <label htmlFor="name" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Discount Name
+              </label>
+              <input
+                type="text"
+                name="name"
+                id="name"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Type discount promo name"
+              />
+            </div>
+            <div className="sm:col-span-2">
+              <label htmlFor="description" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Description
+              </label>
+              <textarea
+                id="description"
+                rows={8}
+                className="block p-2.5 w-full text-sm text-gray-900 bg-gray-50 rounded-lg border border-gray-300 focus:ring-primary-500 focus:border-primary-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Discount description here"
+              ></textarea>
+            </div>
+            <div>
+              <label htmlFor="category" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Discount Type
+              </label>
+              <select
+                id="category"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-500 focus:border-primary-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                onChange={(e) => setDiscountType(e.target.value)}
+              >
+                <option value="">Select Discount Type</option>
+                {discountTypes.map((discountType,index) => (
+                  <option key={index} value={discountType}>
+                    {discountType}
+                  </option>
+                ))}
+              </select>
+            </div>
+            <div className="w-full">
+              <label htmlFor="brand" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Brand
+              </label>
+              <input
+                type="text"
+                name="brand"
+                id="brand"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="Product brand"
+              />
+            </div>
+            <div className="w-full">
+              <label htmlFor="price" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Price
+              </label>
+              <input
+                type="number"
+                name="price"
+                id="price"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="$2999"
+              />
+            </div>
+            <div>
+              <label htmlFor="item-weight" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">
+                Item Weight (kg)
+              </label>
+              <input
+                type="number"
+                name="item-weight"
+                id="item-weight"
+                className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-primary-500 dark:focus:border-primary-500"
+                placeholder="12"
+              />
+            </div>
+          </div>
+          <button
+            type="submit"
+            className="inline-flex items-center px-5 py-2.5 mt-4 sm:mt-6 text-sm font-medium text-center text-white bg-primary-700 rounded-lg focus:ring-4 focus:ring-primary-200 dark:focus:ring-primary-900 hover:bg-primary-800"
+          >
+            Add product
+          </button>
+        </form>
+      </div>
+    </section>
+  );
+}
