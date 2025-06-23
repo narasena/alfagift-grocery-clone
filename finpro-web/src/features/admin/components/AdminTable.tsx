984cfad74032c922c9646c9ec0a55976d78f8c7f
cursemaker <cursedsena@gmail.com>
2025-06-23 18:30:18 +0700

    fix: admin table to have group select

--- finpro-web/src/features/admin/components/AdminTable.tsx
+++ finpro-web/src/features/admin/components/AdminTable.tsx
@@ -18,63 +18,68 @@ interface ICommonAdminTableProps<T> {
 
 interface IAdminTablePropsWithCheckbox<T> extends ICommonAdminTableProps<T> {
   withCheckbox: boolean;
-  getRowId: (row:T) => string
+  getRowId: (row: T) => string;
+  noHeader?: boolean;
   onCheckboxChange?: (checkedRows: string[]) => void;
   checkedRows?: string[];
+  disabledRows?: string[];
 }
 
 interface IAdminTablePropsWithoutCheckbox<T> extends ICommonAdminTableProps<T> {
   withCheckbox?: false;
   getRowId?: never;
+  noHeader?: never;
   onCheckboxChange?: never;
   checkedRows?: never;
+  disabledRows?: never;
 }
 
-export type IAdminTableProps<T> =
-  | IAdminTablePropsWithCheckbox<T>
-  | IAdminTablePropsWithoutCheckbox<T>;
+export type IAdminTableProps<T> = IAdminTablePropsWithCheckbox<T> | IAdminTablePropsWithoutCheckbox<T>;
 
 export default function AdminTable<T extends Record<string, unknown>>(props: IAdminTableProps<T>) {
-  const {
-    title,
-    tableDescription,
-    linkHref,
-    linkLabel,
-    columns,
-    data,
-    renderCell,
-    withCheckbox
-  } = props
+  const { title, tableDescription, linkHref, linkLabel, columns, data, renderCell, withCheckbox } = props;
 
   const [internalChecked, setInternalChecked] = React.useState<string[]>([]);
+  const noHeader = props.noHeader || false;
 
   const isControlled = withCheckbox && Array.isArray(props.checkedRows);
-  const checkedRows:string[] = isControlled ? (props as IAdminTablePropsWithCheckbox<T>).checkedRows! : internalChecked
+  const checkedRows: string[] = isControlled
+    ? (props as IAdminTablePropsWithCheckbox<T>).checkedRows!
+    : internalChecked;
+
+  const disabledRows = withCheckbox ? (props as IAdminTablePropsWithCheckbox<T>).disabledRows || [] : [];
 
   const handleCheckboxChangeInternal = (event: React.ChangeEvent<HTMLInputElement>) => {
     const rowId = event.target.value;
-    const isChecked = event.target.checked 
-    
-    let newCheckedRows: string[]
+    const isChecked = event.target.checked;
+
+    let newCheckedRows: string[];
 
     if (isChecked) {
-      newCheckedRows = [...checkedRows, rowId]
+      newCheckedRows = [...checkedRows, rowId];
     } else {
-      newCheckedRows = checkedRows.filter((id) => id !== rowId)
+      newCheckedRows = checkedRows.filter((id) => id !== rowId);
     }
-    setInternalChecked(newCheckedRows)
-    if(withCheckbox && props.onCheckboxChange) props.onCheckboxChange(newCheckedRows)
-  }
+    setInternalChecked(newCheckedRows);
+    if (withCheckbox && props.onCheckboxChange) props.onCheckboxChange(newCheckedRows);
+  };
   React.useEffect(() => {
     console.log(checkedRows);
-  },[checkedRows])
+  }, [checkedRows]);
   return (
     <div className="relative !overflow-auto shadow-lg sm:rounded-lg">
       <table className="w-full text-sm text-left text-gray-500">
-        <AdminTableHeader title={title} tableDescription={tableDescription} linkHref={linkHref} linkLabel={linkLabel} />
+        {title && (
+          <AdminTableHeader
+            title={title}
+            tableDescription={tableDescription}
+            linkHref={linkHref}
+            linkLabel={linkLabel}
+          />
+        )}
         <thead className="text-xs text-gray-700 uppercase bg-gray-200">
           <tr>
-            {withCheckbox && (
+            {withCheckbox && !noHeader ? (
               <th scope="col" className="p-4">
                 <div className="flex items-center">
                   <input
@@ -82,9 +87,7 @@ export default function AdminTable<T extends Record<string, unknown>>(props: IAd
                     type="checkbox"
                     checked={data.length > 0 && checkedRows.length === data.length}
                     onChange={(event) => {
-                      const allIds = event.target.checked
-                        ? data.map((row) => props.getRowId(row))
-                        : [];
+                      const allIds = event.target.checked ? data.map((row) => props.getRowId(row)) : [];
 
                       setInternalChecked(allIds);
                       if (props.onCheckboxChange) props.onCheckboxChange(allIds);
@@ -96,6 +99,10 @@ export default function AdminTable<T extends Record<string, unknown>>(props: IAd
                   </label>
                 </div>
               </th>
+            ) : (
+              <th scope="col" className="p-4">
+                <div></div>
+              </th>
             )}
             {columns.map((col, index) => (
               <th scope="col" key={index} className="px-6 py-3">
@@ -106,7 +113,7 @@ export default function AdminTable<T extends Record<string, unknown>>(props: IAd
         </thead>
         <tbody>
           {data.map((row, rowIndex) => {
-            const rowId = withCheckbox ? props.getRowId(row): ""
+            const rowId = withCheckbox ? props.getRowId(row) : "";
             return (
               <tr key={rowIndex} className="bg-white border-b border-gray-200 hover:bg-gray-50">
                 {withCheckbox && (
@@ -118,7 +125,10 @@ export default function AdminTable<T extends Record<string, unknown>>(props: IAd
                         checked={checkedRows.includes(rowId)}
                         value={rowId}
                         onChange={handleCheckboxChangeInternal}
-                        className="size-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2"
+                        disabled={disabledRows.includes(rowId)}
+                        className={`size-4 text-blue-600 bg-gray-100 border-gray-300 rounded-sm focus:ring-blue-500 focus:ring-2 ${
+                          disabledRows.includes(rowId) ? 'opacity-50 cursor-not-allowed' : ''
+                        }`}
                       />
                       <label htmlFor={`checkbox-${rowId}`} className="sr-only">
                         checkbox
@@ -132,7 +142,7 @@ export default function AdminTable<T extends Record<string, unknown>>(props: IAd
                   </td>
                 ))}
               </tr>
-            )
+            );
           })}
         </tbody>
       </table>
