404cc42e58b58a31382e32a4940cb4c263e6f12c
cursemaker <cursedsena@gmail.com>
2025-06-24 17:40:52 +0700

    fix: add discount schema

--- finpro-web/src/features/admin/discounts/add/schemas/addDiscountsSchema.ts
+++ finpro-web/src/features/admin/discounts/add/schemas/addDiscountsSchema.ts
@@ -1,4 +1,4 @@
-import { EDiscountValueType } from "@/types/discounts/discount.type";
+import { EDiscountType } from "@/types/discounts/discount.type";
 import * as Yup from "yup";
 
 const addDiscountSchema = Yup.object().shape({
@@ -6,19 +6,17 @@ const addDiscountSchema = Yup.object().shape({
   description: Yup.string().optional(),
   discountType: Yup.string()
     .required("Discount type is required")
-    .oneOf(Object.values(EDiscountValueType), "Invalid discount type"),
+    .oneOf(Object.values(EDiscountType), "Invalid discount type"),
   discountValue: Yup.number()
-    .nullable()
-    .transform((val, orig) => (orig === "" ? null : val))
+    .optional()
     .when(["discountType", "isGlobalProduct"], {
       is: (discountType: string, isGlobalProduct: boolean) =>
         ["PERCENTAGE", "FIXED_AMOUNT"].includes(discountType) && isGlobalProduct,
-      then: (schema) => schema.required("Discount value is required").positive("Must be positive"),
+      then: (schema) => schema.required("Discount value is required").min(50000,"Discount value must be at least 50,000"),
       otherwise: (schema) => schema.strip(),
     }),
   minPurchaseValue: Yup.number()
-    .nullable()
-    .transform((val, origin) => (origin === "" ? null : val))
+    .optional()
     .when("discountType", {
       is: "MIN_PURCHASE",
       then: (schema) =>
@@ -33,25 +31,28 @@ const addDiscountSchema = Yup.object().shape({
     .min(Yup.ref("startDate"), "End date must be after start date"),
   isGlobalProduct: Yup.boolean().optional(),
   isGlobalStore: Yup.boolean().optional(),
-  usageLimitPerTransaction: Yup.number().optional().positive(),
+  usageLimitPerTransaction: Yup.number()
+    .nullable()
+    .transform((val, orig) => (orig === "" ? null : val))
+    .optional(),
   selectedStores: Yup.array().when(["isGlobalStore"], {
     is: false,
     then: (schema) => schema.min(1, "Select at least one store").required(),
     otherwise: (schema) => schema.optional(),
   }),
   selectedProducts: Yup.array().when(["discountType", "isGlobalProduct"], {
-    is: (discountType: string, isGlobalProduct: boolean) => discountType !== "MIN_PURCHASE" && !isGlobalProduct,
+    is: (discountType: string, isGlobalProduct: boolean) => discountType === "BUY1_GET1" && !isGlobalProduct,
     then: (schema) => schema.min(1, "Select at least one product").required(),
     otherwise: (schema) => schema.optional(),
   }),
-  productDiscounts: Yup.array().when(["discountType", "isGlobalProduct"], {
+  toBeDiscountedProducts: Yup.array().when(["discountType", "isGlobalProduct"], {
     is: (discountType: string, isGlobalProduct: boolean) =>
       ["PERCENTAGE", "FIXED_AMOUNT"].includes(discountType) && !isGlobalProduct,
     then: (schema) =>
       schema.min(1, "Add at least one product discount group").of(
         Yup.object().shape({
           discountValue: Yup.number().required("Discount value is required").positive("Must be positive"),
-          productIds: Yup.array().min(1, "Select at least one product").required(),
+          productIds: Yup.array().min(1, "One product is required at least").required(),
         })
       ),
     otherwise: (schema) => schema.optional(),
