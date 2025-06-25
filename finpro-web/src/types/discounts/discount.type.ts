b46b3da5af6ae97ad02f1c0c0114207c70457e3e
cursemaker <cursedsena@gmail.com>
2025-06-25 10:56:56 +0700

    fix: change add discount form type name

--- finpro-web/src/types/discounts/discount.type.ts
+++ finpro-web/src/types/discounts/discount.type.ts
@@ -1,22 +1,22 @@
 export enum EDiscountType {
-    PERCENTAGE = "PERCENTAGE",
-    FIXED_AMOUNT = "FIXED_AMOUNT",
-    BUY1_GET1 = "BUY1_GET1",
-    MIN_PURCHASE = "MIN_PURCHASE"
+  PERCENTAGE = "PERCENTAGE",
+  FIXED_AMOUNT = "FIXED_AMOUNT",
+  BUY1_GET1 = "BUY1_GET1",
+  MIN_PURCHASE = "MIN_PURCHASE",
 }
 export type TDiscountType = keyof typeof EDiscountType;
 
 export interface IDiscountForm {
-    name: string;
-    description?: string;
-    discountType: TDiscountType|"";
-    discountValue?: number;
-    minPurchaseValue?: number;
-    startDate: string;
-    endDate: string;
-    isGlobalProduct: boolean;
-    isGlobalStore: boolean;
-    usageLimitPerTransaction?: number
+  name: string;
+  description?: string;
+  discountType: TDiscountType | "";
+  discountValue?: number;
+  minPurchaseValue?: number;
+  startDate: string;
+  endDate: string;
+  isGlobalProduct: boolean;
+  isGlobalStore: boolean;
+  usageLimitPerTransaction?: number;
 }
 
 export interface IPriceCutSelectedProduct {
@@ -24,10 +24,10 @@ export interface IPriceCutSelectedProduct {
   productIds: string[];
 }
 
-export interface IAddDiscountInitialValues extends IDiscountForm {
-    selectedProducts: string[];
-    selectedStores: string[];
-    toBeDiscountedProducts: IPriceCutSelectedProduct[]
+export interface IAddDiscountForm extends IDiscountForm {
+  selectedProducts: string[];
+  selectedStores: string[];
+  toBeDiscountedProducts: IPriceCutSelectedProduct[];
 }
 
 export interface IDiscount extends IDiscountForm {
@@ -42,40 +42,39 @@ export interface IDiscount extends IDiscountForm {
 }
 
 export interface IVoucherForm {
-    name: string;
-    description?: string;
-    code?: string;
-    voucherType: EVoucherType;
-    discountId?: string
-    discountValue?: number;
-    discountValueType?: EDiscountValueType;
-    maxTotalDiscountValue?: number;
-    userId?: string
-    generatorOrderId?: string
-    storeId?: string
-    referrerId?: string
-    referreeId?: string
-    expiredDate: Date;
-    minTransactionTimes?: number;
-    usageLimitPerUser?: number
-    totalUsageLimit?: number;
+  name: string;
+  description?: string;
+  code?: string;
+  voucherType: EVoucherType;
+  discountId?: string;
+  discountValue?: number;
+  discountValueType?: EDiscountValueType;
+  maxTotalDiscountValue?: number;
+  userId?: string;
+  generatorOrderId?: string;
+  storeId?: string;
+  referrerId?: string;
+  referreeId?: string;
+  expiredDate: Date;
+  minTransactionTimes?: number;
+  usageLimitPerUser?: number;
+  totalUsageLimit?: number;
 }
 
 export interface IVoucher extends IVoucherForm {
-    id: string;
-    createdAt: Date;
-    updatedAt: Date;
-    deletedAt?: Date;
-
+  id: string;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
 }
 
 export enum EVoucherType {
-    PRICE_CUT = "PRICE_CUT",
-    FREE_SHIPPING = "FREE_SHIPPING",
-    REFERRAL = "REFERRAL"
+  PRICE_CUT = "PRICE_CUT",
+  FREE_SHIPPING = "FREE_SHIPPING",
+  REFERRAL = "REFERRAL",
 }
 
-export enum EDiscountValueType{
-    PERCENTAGE = "PERCENTAGE",
-    FIXED_AMOUNT = "FIXED_AMOUNT"
+export enum EDiscountValueType {
+  PERCENTAGE = "PERCENTAGE",
+  FIXED_AMOUNT = "FIXED_AMOUNT",
 }
