cabbc1f29c1a686dcd0be64e55c1bc0304b6d0cc
cursemaker <cursedsena@gmail.com>
2025-06-26 10:54:41 +0700

    fix: store location picker

--- finpro-web/src/hooks/stores/usePickStoreId.ts
+++ finpro-web/src/hooks/stores/usePickStoreId.ts
@@ -3,39 +3,37 @@ import storeLocationStore from "@/zustand/storeLocation.store";
 import { useAllStores } from "./useAllStores";
 
 export default function usePickStoreId() {
-    const { stores } = useAllStores();
-    const setLocationStore = storeLocationStore((state) => state.setStoreLocation);
-    const localStorageStoreId = storeLocationStore((state) => state.storeId);
-    
-    const [storeId, setStoreId] = React.useState(localStorageStoreId);
-    const [miniButton, setMiniButton] = React.useState(true);
-    const [dropdown, setDropdown] = React.useState(false);
-    const hasInitialized = React.useRef(false);
+  const { stores } = useAllStores();
+  const setLocationStore = storeLocationStore((state) => state.setStoreLocation);
+  const localStorageStoreId = storeLocationStore((state) => state.storeId);
 
-    const handleInitialLocationStore = () => {
-        if(!localStorageStoreId) {
-            setLocationStore({ _latitude: null, _longitude: null, _storeId: "cmbrpl9tp0002p1oo9ca2exgc" });
-        } else {
-            setStoreId(localStorageStoreId);
-        }
-        console.log(localStorageStoreId);
-    }
+  const [storeId, setStoreId] = React.useState(localStorageStoreId);
+  const [miniButton, setMiniButton] = React.useState(true);
+  const [dropdown, setDropdown] = React.useState(false);
+  const hasInitialized = React.useRef(false);
 
-    React.useEffect(() => {
-        if (!hasInitialized.current) {
-            handleInitialLocationStore();
-            hasInitialized.current = true;
-        }
-    }, [localStorageStoreId]);
-    
-    return {
-        stores,
-        storeId,
-        setStoreId,
-        setLocationStore,
-        miniButton,
-        setMiniButton,
-        dropdown,
-        setDropdown
+  const handleInitialLocationStore = () => {
+    if (!localStorageStoreId) {
+      setLocationStore({ _latitude: null, _longitude: null, _storeId: "cmbrpl9tp0002p1oo9ca2exgc" });
     }
+  };
+
+  React.useEffect(() => {
+    if (!hasInitialized.current) {
+      handleInitialLocationStore();
+      hasInitialized.current = true;
+    }
+    setStoreId(localStorageStoreId);
+  }, [localStorageStoreId]);
+
+  return {
+    stores,
+    storeId,
+    setStoreId,
+    setLocationStore,
+    miniButton,
+    setMiniButton,
+    dropdown,
+    setDropdown,
+  };
 }
