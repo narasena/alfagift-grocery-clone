06657062b04d2b19ae8bbe625947839f203c8479
cursemaker <cursedsena@gmail.com>
2025-06-27 17:27:16 +0700

    fix: comment token log in jwtDecode

--- finpro-api/src/middlewares/jwt.decode.ts
+++ finpro-api/src/middlewares/jwt.decode.ts
@@ -5,7 +5,7 @@ export const jwtDecode = (req: Request, res: Response, next: NextFunction) => {
   try {
     const token = req.headers.authorization?.split(" ")[1];
 
-    console.log("Token:", token); // Debugging line to check the token
+    // console.log("Token:", token); // Debugging line to check the token
 
     if (!token || token === "null") {
       throw { isExpose: true, status: 401, message: "Token must be provide" };
