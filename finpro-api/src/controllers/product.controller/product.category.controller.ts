8a641c25eeb655816455e35633bf3dfc7e6fa63e
cursemaker <cursedsena@gmail.com>
2025-06-28 14:12:16 +0700

    fix: subCategories and categories controller handler

--- finpro-api/src/controllers/product.controller/product.category.controller.ts
+++ finpro-api/src/controllers/product.controller/product.category.controller.ts
@@ -5,6 +5,14 @@ export class ProductCategoryController {
   async getProductSubCategories(req: Request, res: Response, next: NextFunction) {
     try {
       const productSubCategories = await prisma.productSubCategory.findMany();
+      if (productSubCategories.length === 0) {
+        throw {
+          isExpose: true,
+          success: false,
+          status: 404,
+          message: "Data not found",
+        };
+      }
       res.status(200).json({
         success: true,
         message: "Get data successfull",
@@ -18,6 +26,14 @@ export class ProductCategoryController {
   async getProductCategories(req: Request, res: Response, next: NextFunction) {
     try {
       const productCategories = await prisma.productCategory.findMany();
+      if (productCategories.length === 0) {
+        throw {
+          isExpose: true,
+          success: false,
+          status: 404,
+          message: "Data not found",
+        };
+      }
       res.status(200).json({
         success: true,
         message: "Get data successfull",
@@ -27,4 +43,6 @@ export class ProductCategoryController {
       next(error);
     }
   }
+
+  async createProductCategory(req: Request, res: Response, next: NextFunction) {}
 }
