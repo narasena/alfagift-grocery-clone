ca162819bfab42e024a7ed5bc58d6c0b1bc13070
cursemaker <cursedsena@gmail.com>
2025-06-29 13:10:31 +0700

    fix: users and admins panel dashboard

--- finpro-web/src/app/(admin)/admins/page.tsx
+++ finpro-web/src/app/(admin)/admins/page.tsx
@@ -2,35 +2,41 @@
 import { useAdminsAdmin } from "@/features/admin/admins/hooks/useAdminsAdmin";
 import ActiveTabs from "@/features/admin/components/ActiveTabs";
 import AdminPageTitle from "@/features/admin/components/AdminPageTitle";
+import AdminTable from "@/features/admin/components/AdminTable";
+import { IAdminTable } from "@/types/admins/admin.type";
+import { IUserTable } from "@/types/users/user.type";
 import * as React from "react";
-import { HiUserGroup } from "react-icons/hi";
-import { RiAdminFill } from "react-icons/ri";
 
 export default function AdminAdminsManagerPage() {
-  const { tabHeaders, activeTab, setActiveTab } = useAdminsAdmin();
-  
+  const {
+    tabHeaders,
+    activeTab,
+    setActiveTab,
+    admins,
+    adminTableTitles,
+    renderAdminsCell,
+    users,
+    userTableTitles,
+    renderUsersCell,
+  } = useAdminsAdmin();
 
   return (
     <div>
       <AdminPageTitle title="Admins & Users Manager" subTitle="Users and Admins Management Panel" />
       <div className="p-4 my-2 gap-4 border border-gray-700 rounded-lg">
         {/* title tabs */}
-        <ActiveTabs 
-          tabHeaders={tabHeaders} 
-          defaultTab="admins"
-          onTabChange={(activeTab) => setActiveTab(activeTab)} 
-        />
-        
+        <ActiveTabs tabHeaders={tabHeaders} defaultTab="admins" onTabChange={(activeTab) => setActiveTab(activeTab)} />
+
         {/* Conditional rendering based on active tab */}
         {activeTab === "admins" && (
           <div className="mt-4 p-4 bg-blue-100 rounded">
-            <h3>Admins Content</h3>
+            <AdminTable columns={adminTableTitles} data={admins as IAdminTable[]} renderCell={renderAdminsCell} />
           </div>
         )}
-        
+
         {activeTab === "users" && (
           <div className="mt-4 p-4 bg-green-100 rounded">
-            <h3>Users Content</h3>
+            <AdminTable columns={userTableTitles} data={users as IUserTable[]} renderCell={renderUsersCell} />
           </div>
         )}
       </div>
