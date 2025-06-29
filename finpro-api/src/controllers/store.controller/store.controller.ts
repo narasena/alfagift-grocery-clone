0dd45e3eb53a202f178ad7e5db7794f2b7a3ccc8
cursemaker <cursedsena@gmail.com>
2025-06-29 19:54:25 +0700

    feat: register with referral code & some fixes

--- finpro-api/src/controllers/store.controller/store.controller.ts
+++ finpro-api/src/controllers/store.controller/store.controller.ts
@@ -1,7 +1,7 @@
 import { prisma } from "../../prisma";
 import { NextFunction, Request, Response } from "express";
 
-export const getAllStores = async (req: Request, res: Response, next: NextFunction) => {
+export const fetchAllStores = async (req: Request, res: Response, next: NextFunction) => {
     try {
         const stores = await prisma.store.findMany({where: {deletedAt: null}});
         res.status(200).json({
