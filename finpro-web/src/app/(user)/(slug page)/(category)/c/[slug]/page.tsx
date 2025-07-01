91087ec2d25f22505b9a5a340a8c7be9bff8439d
cursemaker <cursedsena@gmail.com>
2025-07-01 09:21:41 +0700

    feat: categories slug page

--- finpro-web/src/app/(user)/(slug page)/(category)/c/[slug]/page.tsx
+++ finpro-web/src/app/(user)/(slug page)/(category)/c/[slug]/page.tsx
@@ -1,23 +1,24 @@
-'use client'
-import { useCategory } from '@/features/(user)/c/hooks/useCategory';
-import * as React from 'react';
-import { IoIosArrowDroprightCircle } from 'react-icons/io';
-import Link from 'next/link';
+"use client";
+import { useCategory } from "@/features/(user)/c/hooks/useCategory";
+import * as React from "react";
+import { IoIosArrowDroprightCircle } from "react-icons/io";
+import Link from "next/link";
+import { CldImage } from "next-cloudinary";
+import { IoStorefront } from "react-icons/io5";
+import { RiTimerFlashFill } from "react-icons/ri";
 
-export interface IAppProps {
-}
+export interface IAppProps {}
 
 export default function CategorySlugPage(props: IAppProps) {
-    const { category, breadcrumbLinks } = useCategory();
-    console.log(category);
-    console.log(breadcrumbLinks);
+  const { category, breadcrumbLinks, products, storeId } = useCategory();
+ 
   return (
-    <div>
+    <div className="sm:max-w-[960px] mx-auto p-[15px]">
       {/* Breadcrumb */}
-      <div className="hidden lg:flex lg:border lg:border-gray-100 items-center px-6 py-4 rounded-full w-full shadow-md my-2">
+      <div className="hidden sm:flex sm:border sm:border-gray-100 items-center px-6 py-4 rounded-full w-full shadow-md my-2">
         {breadcrumbLinks.map((link, index) => (
           <React.Fragment key={index}>
-            {link.href === '#' ? (
+            {link.href === "#" ? (
               <span className="text-xs text-gray-500">{link.label}</span>
             ) : (
               <Link href={link.href} className="text-xs text-gray-500 hover:text-red-700">
@@ -30,6 +31,76 @@ export default function CategorySlugPage(props: IAppProps) {
               </span>
             )}
           </React.Fragment>
+        ))}
+      </div>
+      {/* page title */}
+      <div className="w-full flex flex-col gap-1.5 my-6">
+        <div className="flex flex-col">
+          <span className="text-xl tracking-tight font-bold text-gray-800">{category?.name}</span>
+        </div>
+        <div className="mb-4 text-xs text-[#999999]">
+          <span>{`Menampilkan ${products.length} produk`}</span>
+        </div>
+      </div>
+      {/* cards */}
+      <div className="flex flex-wrap w-full">
+        {products.map((product) => (
+          <div className="sm:max-w-[16.667%] sm:max-h-[400px] sm:px-[15px]">
+            <Link href={`/p/${product.slug}`} className="block rounded-md shadow-md mb-6">
+              <div className="flex flex-col ">
+                <div className="overflow-hidden centered">
+                  <CldImage src={product.productImage[0].imageUrl} width={144} height={144} alt={product.name} />
+                </div>
+                <div className="px-2 text-sm h-[45px] overflow-hidden text-ellipsis">{product.name}</div>
+                <div className="p-2">
+                  <div className="h-5 flex items-center">
+                    {product.productDiscountHistories.length > 0 && (
+                      <span className="text-[10px] h-max line-through text-[#999999]">
+                        {product.price.toLocaleString("id-ID", {
+                          style: "currency",
+                          currency: "IDR",
+                          minimumFractionDigits: 0,
+                        })}
+                      </span>
+                    )}
+                  </div>
+                  <div>
+                    <span className="font-bold text-base text-red-600">
+                      {product.productDiscountHistories.length > 0
+                        ? (
+                            product.price *
+                            (1 - product.productDiscountHistories[0].discountValue / 100)
+                          ).toLocaleString("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                            minimumFractionDigits: 0,
+                          })
+                        : product.price.toLocaleString("id-ID", {
+                            style: "currency",
+                            currency: "IDR",
+                            minimumFractionDigits: 0,
+                          })}
+                    </span>
+                  </div>
+                </div>
+                <div className="p-2 flex flex-col gap-1 *:min-h-3 *:w-full text-[10px]">
+                  <div className="flex items-center gap-2">
+                    <IoStorefront className="text-red-600 text-sm" />
+                    <span className="italic text-[#999999]">Produk Online</span>
+                  </div>
+                  <div className="flex items-center gap-2">
+                    <RiTimerFlashFill className="text-red-600 text-sm" />
+                    <span className="font-bold text-red-600">Pengiriman Instan</span>
+                  </div>
+                </div>
+              </div>
+              <div className="p-2 border-t border-gray-300">
+                <button className="py-2 w-full bg-red-600 rounded-md font-medium text-white hover:bg-red-700">
+                  Beli
+                </button>
+              </div>
+            </Link>
+          </div>
         ))}
       </div>
     </div>
