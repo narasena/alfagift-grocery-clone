6a232dc93048ffb6c5aa87499eee35f9610f49dc
cursemaker <cursedsena@gmail.com>
2025-07-01 20:23:52 +0700

    feat: discounts overview page

--- finpro-web/src/app/(admin)/discounts/page.tsx
+++ finpro-web/src/app/(admin)/discounts/page.tsx
@@ -1,11 +1,22 @@
+'use client'
+
+import AdminTable from '@/features/admin/components/AdminTable';
+import { useAdminDiscount } from '@/features/admin/discounts/hooks/useAdminDiscount';
+import { IDiscountResponseTable } from '@/types/discounts/discount.type';
 import * as React from 'react';
 
 
 export default function AdminDiscountPage() {
+  const { discounts,discountsTitle,renderDiscountsCell } = useAdminDiscount()
+  console.log(discounts)
     
   return (
     <div>
-      
+      <AdminTable 
+        columns={discountsTitle}
+        data={discounts as IDiscountResponseTable[]}
+        renderCell={renderDiscountsCell}
+      />
     </div>
   );
 }
