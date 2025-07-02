ca3cadfc2cd4ac0cd75be71e07d292fefde5cae3
cursemaker <cursedsena@gmail.com>
2025-07-02 14:28:26 +0700

    feat: order and order item types

--- finpro-web/src/types/orders/orders.type.ts
+++ finpro-web/src/types/orders/orders.type.ts
@@ -7,3 +7,33 @@ export type OrderCardProps = {
   finalTotalAmount: number;
   onDetailClick?: () => void; // optional, or you can handle inside
 };
+
+export interface IOrderDetails {
+  id: string;
+  userId: string;
+  storeId: string;
+  shippingAddressId: string;
+  totalAmount: number;
+  discountedTotalAmount: number;
+  finalTotalAmount: number;
+  shippingCost: number;
+  discountedshippingCost: number;
+  finalshippingCost: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
+}
+
+export interface IOrderItemDetails {
+  id: string;
+  orderId: string;
+  productId: string;
+  discountId?: string;
+  quantity: number;
+  originalPrice: number;
+  discountedPrice: number;
+  finalPrice: number;
+  createdAt: Date;
+  updatedAt: Date;
+  deletedAt?: Date;
+}
