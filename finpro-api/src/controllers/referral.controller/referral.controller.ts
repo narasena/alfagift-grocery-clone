b35b9e56eb737499d1cd091ec376c9582c2e3ab0
narasena <cursedsena@gmail.com>
2025-09-02 10:30:14 +0700

    refactor: referrals

--- finpro-api/src/controllers/referral.controller/referral.controller.ts
+++ finpro-api/src/controllers/referral.controller/referral.controller.ts
@@ -1,30 +1,18 @@
 import { NextFunction, Request, Response } from "express";
-import { prisma } from '../../prisma'
+import ReferralService from "@/services/referrals/referral.service";
 
 export async function findReferral(req: Request, res: Response, next: NextFunction) {
-    try {
-        const { referralCode } = req.params
-        const user = await prisma.user.findUnique({
-            where: {
-                referralCode
-            }
-        })
-        if(!user) {
-            throw {
-              isExpose: true,
-              status: 400,
-              success: false,
-              message: "User with this referral code not found",
-            };
-        }
+  try {
+    const { referralCode } = req.params;
+    const referralService = new ReferralService();
+    const user = await referralService.findUserReferral(referralCode);
 
-        res.status(200).json({
-          success: true,
-          message: "Referral code found",
-          referralCode: user.referralCode,
-        });
-        
-    } catch (error) {
-        next(error)
-    }
+    res.status(200).json({
+      success: true,
+      message: "Referral code found",
+      referralCode: user.referralCode,
+    });
+  } catch (error) {
+    next(error);
+  }
 }
---
+++ finpro-api/src/services/referrals/referral.service.ts
@@ -0,0 +1,24 @@
+import { prisma } from "../../prisma"
+
+export default class ReferralService {
+  public async findUserReferral(referralCode: string) {
+    try {
+      const user = await prisma.user.findUnique({
+        where: {
+          referralCode
+        }
+      })
+      if (!user) {
+        throw {
+          isExpose: true,
+          status: 404,
+          success: false,
+          message: "User with this referral code not found",
+        };
+      }
+      return user
+    } catch (error) {
+      throw error
+    }
+  }
+}
