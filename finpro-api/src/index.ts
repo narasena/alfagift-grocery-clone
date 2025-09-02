95546b45e7ee406332907fc6a78f6e0307fb0363
narasena <cursedsena@gmail.com>
2025-09-02 12:27:00 +0700

    fix: vercel deployment 4

--- finpro-api/src/index.ts
+++ finpro-api/src/index.ts
@@ -5,10 +5,11 @@ import App from './app';
 
 const app = new App();
 
-// For Vercel serverless
-export default app.getApp();
-
 // For local development
 if (process.env.NODE_ENV !== 'production') {
   app.start();
 }
+
+// For Vercel serverless - export the Express app
+module.exports = app.getApp();
+export default app.getApp();
